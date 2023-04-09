7ca2c7fd4ff6469a5052a46c3892c72f0d177d48
miguelnunez15 <miguel.nunez.rodriguez@estudiantat.upc.edu>
2023-04-09 17:36:53 +0200

    update equipo + desasignar tarea

--- backend/polls/views/issue_viewset.py
+++ backend/polls/views/issue_viewset.py
@@ -8,32 +8,51 @@ import datetime
 
 # Mostrar pantalla de creación de un issue
 def pantallaCrearIssue(request):
+    usuario = User.objects.get(id=request.user.id)
 
-    equipo = Miembro_Equipo.objects.filter(miembro=request.user)
-    miebros = Miembro_Equipo.objects.filter(equipo=equipo[0].equipo)
-    usuarios = []
-    for miembro in miebros:
-        usuarios.append(miembro.miembro)
+    if Miembro_Equipo.objects.filter(miembro=usuario):
+        equipo = Miembro_Equipo.objects.filter(miembro=request.user)
+        miebros = Miembro_Equipo.objects.filter(equipo=equipo[0].equipo)
+        usuarios = []
+        for miembro in miebros:
+            usuarios.append(miembro.miembro)
 
-    return render(request, 'crearIssue.html', {'usuarios' : usuarios})
+        sinAsignar = User(username="sin asignar")
+        usuarios.append(sinAsignar)
+        return render(request, 'crearIssue.html', {'usuarios' : usuarios})
+    else:
+        return render(request, 'crearIssue.html')
 
 # Crear un nuevo issue
 def crearIssue(request):
 
+
+    # Obtengo los miembros
+    equipo = Miembro_Equipo.objects.filter(miembro=request.user)
+    usuarios = []
+    if equipo:
+        miembros = Miembro_Equipo.objects.filter(equipo=equipo[0].equipo)
+        for miembro in miembros:
+            usuarios.append(miembro.miembro)
+        sinAsignar = User(username="sin asignar")
+        usuarios.append(sinAsignar)
+
     if request.method == 'GET':
-        asunto = request.GET.get('asunto')
-        descripcion = request.GET.get('descripcion')
-        creador = User.objects.get(id=request.user.id)
-        issue = Issue(asunto=asunto, descripcion=descripcion, creador=creador)
-        issue.save()
+        if request.GET.get('asunto') != '':
+            asunto = request.GET.get('asunto')
+            descripcion = request.GET.get('descripcion')
+            creador = User.objects.get(id=request.user.id)
+            issue = Issue(asunto=asunto, descripcion=descripcion, creador=creador)
+            issue.save()
         
-        actividad = Actividad_Issue(issue=issue, creador=issue.creador, fecha=datetime.datetime.now(), tipo="creada", usuario=request.user)
-        actividad.save()
-        #associat = request.POST.get('associat')
-        #vigilant = request.POST.get('vigilant'
-    
+            actividad = Actividad_Issue(issue=issue, creador=issue.creador, fecha=datetime.datetime.now(), tipo="creada", usuario=request.user)
+            actividad.save()
+            #associat = request.POST.get('associat')
+            #vigilant = request.POST.get('vigilant'
+        else:
+            return render(request, 'crearIssue.html', {'error' : 'El asunto no puede estar vacío', 'usuarios' : usuarios})
         
-    return render(request, 'crearIssue.html')
+    return render(request, 'crearIssue.html', {'error' : "Se ha creado el issue correctamente", 'usuarios' : usuarios})
 
 # Mostrar la información del issue dado su id
 def mostrarIssue(request, idIssue):
@@ -57,7 +76,7 @@ def mostrarPantallaEditarIssue(request, idIssue):
     issue = Issue.objects.get(id=idIssue)
     creador = User.objects.get(id=issue.creador.id)
 
-    # Si el miembro tiene equipo
+    
 
     if Miembro_Equipo.objects.filter(miembro=creador):
         equipo = Miembro_Equipo.objects.filter(miembro=creador)[0].equipo
@@ -65,9 +84,11 @@ def mostrarPantallaEditarIssue(request, idIssue):
         aux = []
         for miembro in miembros:
             aux.append(miembro.miembro)
+        sinAsignar = User(username="sin asignar")
+        aux.append(sinAsignar)
         return render(request, 'editarIssue.html', {'issue': issue, 'creador' : creador, 'usuarios' : aux })
 
-    return render(request, 'editarIssue.html', {'issue': issue, 'creador' : creador })
+    return render(request, 'editarIssue.html', {'issue': issue, 'creador' : creador})
 
 # Acualizar el issue con la nueva información
 def editarIssue(request, idIssue):
@@ -89,13 +110,29 @@ def editarIssue(request, idIssue):
             issue.descripcion = request.GET.get('descripcion')
             issue.save()
 
-        if request.GET.get('asignada'):
-            actividad = Actividad_Issue(issue=issue, creador=issue.creador, fecha=datetime.datetime.now(), tipo="asignada", usuario=request.user)
-            actividad.save()
-            username_asignado = request.GET.get('asignada')
-            user = User.objects.get(username=username_asignado)
-            issue.asignada = user
-            issue.save()
+
+        puedo_asignar = False
+        print("issue.asignada: ", issue.asignada)
+        if issue.asignada == None :
+            puedo_asignar = True
+        elif request.GET.get('asignada') != issue.asignada.username and request.GET.get('asignada') != '':
+            puedo_asignar = True
+         
+
+        if  puedo_asignar:
+            if request.GET.get('asignada') == "sin asignar":
+                if issue.asignada != None:
+                    actividad = Actividad_Issue(issue=issue, creador=issue.creador, fecha=datetime.datetime.now(), tipo="desasignada", usuario=request.user)
+                    actividad.save()
+                issue.asignada = None
+                issue.save()
+            else:
+                actividad = Actividad_Issue(issue=issue, creador=issue.creador, fecha=datetime.datetime.now(), tipo="asignada", usuario=request.user)
+                actividad.save()
+                username_asignado = request.GET.get('asignada')
+                user = User.objects.get(username=username_asignado)
+                issue.asignada = user
+                issue.save()
 
         actividades = Actividad_Issue.objects.filter(issue_id=idIssue)
         return render(request, 'mostrarIssue.html', {'issue': issue, 'actividades' : actividades})
