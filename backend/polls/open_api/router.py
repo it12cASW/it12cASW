eb225254c589e47d1caed4bbe80b88121a47694d
miguelnunez15 <miguel.nunez.rodriguez@estudiantat.upc.edu>
2023-05-12 22:09:24 +0200

    Clases para api divididas en diferentes archivos. FIN CONF API

--- backend/polls/open_api/router.py
+++ backend/polls/open_api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from polls.open_api.views import ComentarioViewSet
+from polls.open_api.views.comment_view import ComentarioViewSet
 
 router = DefaultRouter()
 router.register('comentarios', ComentarioViewSet, basename='comentarios')
--- backend/polls/open_api/views/comment_view.py
+++ backend/polls/open_api/views/comment_view.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from polls.models import Comentario
-from polls.open_api.serializers import ComentarioSerializer
+from polls.open_api.serializers.comment_serializer import ComentarioSerializer
 
 
 class ComentarioViewSet(ModelViewSet):
