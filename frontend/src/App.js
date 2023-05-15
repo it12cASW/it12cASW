52da040559ee84eb670ddec3aa626177d645e708
miguelnunez15 <miguel.nunez.rodriguez@estudiantat.upc.edu>
2023-05-15 06:49:06 +0200

    V2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './views/Login/Login';
-import Register from './views/Register/Register';
 import Panel from './views/Panel/Panel';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/panel" element={<Panel />} />
+      <Route path="/" element={<Panel />} />
     </Routes>
   );
 }
