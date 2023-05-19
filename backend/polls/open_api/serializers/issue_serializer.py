16282fb11a5cee7ada24520b8227331aa501e1f5
Juan Clusellas <juanclusellas@MacBook-Pro-de-Juan.local>
2023-05-19 10:06:39 +0200

    activities done (inside serializer) and on viewset

--- backend/polls/open_api/serializers/issue_serializer.py
+++ backend/polls/open_api/serializers/issue_serializer.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
-from polls.models import Issue
+from polls.models import Issue , Actividad_Issue
 from .user_serializer import UserSerializer
+
+
+class Actividad_IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actividad_Issue
+        fields = '__all__'
 
 class IssueSerializer(serializers.ModelSerializer):
     creador = UserSerializer()
     associat = UserSerializer()
     vigilant = UserSerializer(many=True)
     asignada = UserSerializer()
+    actividades = Actividad_IssueSerializer(many=True)
 
     class Meta:
         model = Issue
